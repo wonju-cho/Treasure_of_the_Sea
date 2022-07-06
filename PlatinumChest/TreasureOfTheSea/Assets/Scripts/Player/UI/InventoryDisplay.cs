2619ed6f5a05c4ba09022fefcc8e3b836dd2b1a2
WonJu Jo <wonju.cho@digipen.edu>
2022-07-06 03:18:41 -0700

    fix error(adding start)

--- PlatinumChest/TreasureOfTheSea/Assets/Scripts/Player/UI/InventoryDisplay.cs
+++ PlatinumChest/TreasureOfTheSea/Assets/Scripts/Player/UI/InventoryDisplay.cs
@@ -10,7 +10,10 @@ public abstract class InventoryDisplay : MonoBehaviour
 
     public InventorySystem InventorySystem => inventorySystem;
     public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
+    protected virtual void Start()
+    {
 
+    }
     public abstract void AssignSlot(InventorySystem invToDisplay);
 
     protected virtual void UpdateSlot(InventorySlot updatedSlot)
